338e52fea50fd18813d135eaa5008ddea5da258c
sumitkr19 <103182846+sumitkr19@users.noreply.github.com>
2023-06-14 01:01:59 +0530

    Challenge

--- Q2.C
+++ Q2.C
@@ -1,1 +1,29 @@
-UUHDUE
+//Write a program to print the grade of the given student using if else if block
+#include <stdio.h>
+
+void printGrade(int marks) {
+    if (marks >= 90 && marks <= 100) {
+        printf("Grade A\n");
+    } else if (marks >= 75 && marks <= 89) {
+        printf("Grade B\n");
+    } else if (marks >= 60 && marks <= 74) {
+        printf("Grade C\n");
+    } else if (marks >= 50 && marks <= 59) {
+        printf("Grade D\n");
+    } else if (marks >= 0 && marks <= 49) {
+        printf("Grade F\n");
+    } else {
+        printf("Invalid marks\n");
+    }
+}
+
+int main() {
+    int studentMarks;
+
+    printf("Enter the marks: ");
+    scanf("%d", &studentMarks);
+
+    printGrade(studentMarks);
+
+    return 0;
+}
---
+++ Q3.C
@@ -0,0 +1,41 @@
+// Calculate the total, percentage and print the summary.
+#include <stdio.h>
+
+int main() {
+    int rollNo;
+    char name[100];
+    float physicsMarks, mathMarks, chemistryMarks;
+    float totalMarks, percentage;
+
+    // Read input from user
+    printf("Enter Roll No: ");
+    scanf("%d", &rollNo);
+
+    printf("Enter Name: ");
+    scanf(" %[^\n]", name);
+
+    printf("Enter Marks of Physics: ");
+    scanf("%f", &physicsMarks);
+
+    printf("Enter Marks of Math: ");
+    scanf("%f", &mathMarks);
+
+    printf("Enter Marks of Chemistry: ");
+    scanf("%f", &chemistryMarks);
+
+    // Calculate total marks and percentage
+    totalMarks = physicsMarks + mathMarks + chemistryMarks;
+    percentage = (totalMarks / 300.0) * 100.0;
+
+    // Print the summary
+    printf("\n------ Student Summary ------\n");
+    printf("Roll No: %d\n", rollNo);
+    printf("Name: %s\n", name);
+    printf("Physics Marks: %.2f\n", physicsMarks);
+    printf("Math Marks: %.2f\n", mathMarks);
+    printf("Chemistry Marks: %.2f\n", chemistryMarks);
+    printf("Total Marks: %.2f\n", totalMarks);
+    printf("Percentage: %.2f%%\n", percentage);
+
+    return 0;
+}
